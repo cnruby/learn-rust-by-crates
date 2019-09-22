106783aead3bab13464b645c7fb643618ce0efcc
Gudao Luo <gudao.luo@gmail.com>
2019-09-22 21:51:51 +0200

    trait_exerci = "0.1.1"

---
+++ docs/hello-trait/implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["trait_exerci"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"trait_exerci/struct.StructType.html\" title=\"struct trait_exerci::StructType\">StructType</a>",synthetic:true,types:["trait_exerci::StructType"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
