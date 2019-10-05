5759e388ba9375fd158814bcf886c339fa9fbc48
Gudao Luo <gudao.luo@gmail.com>
2019-10-05 07:20:14 +0200

    hello_exercism = "0.4.10"

---
+++ utils/ci/script.sh
@@ -0,0 +1,31 @@
+# Derived from https://github.com/japaric/trust
+
+set -ex
+
+# ----- Options -----
+
+# TARGET enables cross-building
+if [ -z $TARGET ]; then
+    CARGO=cargo
+elif [ "$TARGET" = "i686-unknown-linux-musl" ]; then
+    CARGO=cargo
+    TARGET="--target $TARGET"
+else
+    CARGO=cross
+    TARGET="--target $TARGET"
+fi
+
+# ALLOC defaults on; is disabled for rustc < 1.36
+if [ -z $ALLOC ]; then
+    ALLOC=1
+fi
+
+# NIGHTLY defaults off
+
+
+# ----- Script -----
+
+main() {
+  cd hello-world && $CARGO build $TARGET --verbose
+  cd hello-world && $CARGO test $TARGET
+}
