a8ae799651fa26f17c0d35fb0e6871400b54c7b5
Gudao Luo <gudao.luo@gmail.com>
2019-10-18 20:32:18 +0200

    mod_trait_exerci = "0.2.0"

--- hello-mod-trait/bin-hello/src/main.rs
+++ hello-mod-trait/bin-hello/src/main.rs
@@ -1,19 +1,19 @@
 fn main() {
     use mod_trait_exerci::mod_trait;
-    let instance = mod_trait::StructType { data: 10 };
-    println!("{}", instance.get());
-    let instance = Box::new(mod_trait::StructType { data: 10 });
-    println!("{}", instance.get());
+    let instance = mod_trait::StructType::new(10);
+    println!("{}", instance.get_tuple());
+    let instance = Box::new(mod_trait::StructType::new(10));
+    println!("{}", instance.get_tuple());
 
     use mod_trait_exerci::mod_bare;
     let instance = mod_bare::StructType { data: 10 };
-    println!("{}", instance.get());
-    let instance = Box::new(mod_bare::StructType { data: 20 });
-    println!("{}", instance.get());
+    println!("{}", instance.get_tuple());
+    let instance = Box::new(mod_bare::StructType { data: 10 });
+    println!("{}", instance.get_tuple());
 
     use mod_trait::TraitCanal as ModTraitCanal;
-    let instance = mod_trait::StructType { data: 100 };
-    println!("{}", instance.get());
-    let instance = Box::new(mod_trait::StructType { data: 200 });
-    println!("{}", instance.get());
+    let instance = mod_trait::StructType::new(10);
+    println!("{}", instance.get_tuple());
+    let instance = Box::new(mod_trait::StructType::new(10));
+    println!("{}", instance.get_tuple());
 }
